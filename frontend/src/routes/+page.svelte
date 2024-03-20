806d61fdf350e01f2d902f408cb1117c2c3c15da
mk <evanxmk@gmail.com>
2024-03-20 13:18:43 +0800

    added components, css

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,58 @@
 <script lang="ts">
-    import { main } from '../script/main'; // It might not be wise to run this as a server side function 
+    // let uploadedImage: File | null = null;
     let input: string = '';
+    let output: string = '';
+
+    // filler function
+    function process_onclick(): void {
+        output = `processed ${input}`;
+    }
 </script>
 
-<h1>Selenium OCR scraper</h1>
+<div class="title">
+    <h1>Selenium OCR scraper</h1>
+</div>
 
-<input type="text" bind:value={input} placeholder="Enter Link"> 
-<button on:click={main}>Submit</button>
+<div class="main">
+    <input type="text" bind:value={input} placeholder="Enter Link"> 
+    <button on:click={process_onclick}>Submit</button>
+    <!-- <p>{output}</p> -->
+</div>
+
+<div class="ret">
+    <p>{output}</p>
+</div>
+
+
+
+<style>
+.title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 30vh;
+    height: 10%;
+    margin: 0;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 10vh;
+  height: 100%;
+  margin: 0;
+}
+
+.ret {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 10vh;
+    height: 100%;
+    margin: 0;
+}
+</style>
